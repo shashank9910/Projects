9626c84a672af48a540d6a06804da1e2f0a7dcdc
Shashank Srivastava <shashank.giant99@gmail.com>
2022-09-20 09:58:16 +0530

    Initial commit

---
+++ JavaBasics.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.math.sqrt;
+import java.util.Arrays;
+
+public class JavaBasics{
+    
+     public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n =sc.nextInt();
+        
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if((i+j)%2!=0){
+                    
+                }
+            }
+            System.out.println();
+        }
+        
+
+    }
+}
+   
+
