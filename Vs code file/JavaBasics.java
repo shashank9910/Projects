15cfb4b6610e18c5451be0b9a05e00dd06112206
Shashank Srivastava <shashank.giant99@gmail.com>
2022-09-24 09:52:50 +0530

    First Commit

---
+++ Vs code file/JavaBasics.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.math.sqrt;
+import java.util.Arrays;
+
+public class JavaBasics{
+    
+     public static void main(String args[])
+    {
+       
+
+
+
+        int ans[]=new int[1];
+    int data[]=new int[input2];
+    comb(input3,input1,input2,0,data,0,ans);
+
+    return ans[0];
+
+
+
+
+        
+
+    }
+}
+   
+
